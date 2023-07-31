c8bb71d74afaae9e361028ba7b18c13c7e3505d3
kits26kat24 <k.tijani@hotmail.com>
2023-07-31 12:26:56 +0100

    Test 5B Project backend tfvars Wip

--- Code/Instance.tf
+++ Code/Instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "Node_1" {
   key_name               = var.ec2_key_name
   subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.ec2_test5b_sg.id]
-  tags                   = var.tags
+  tags                   = var.ec2_name_1
 }
 resource "aws_instance" "Node_2" {
 
@@ -14,7 +14,7 @@ resource "aws_instance" "Node_2" {
   key_name               = var.ec2_key_name
   subnet_id              = aws_subnet.public_2.id
   vpc_security_group_ids = [aws_security_group.ec2_test5b_sg.id]
-  tags                   = var.tags_1
+  tags                   = var.ec2_name_2
 }
 
 resource "aws_instance" "MariaDB" {
@@ -24,7 +24,7 @@ resource "aws_instance" "MariaDB" {
   key_name               = var.ec2_key_name
   subnet_id              = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.ec2_test5b_sg.id]
-  tags                   = var.tags_2
+  tags                   = var.ec2_name_3
 }
 
 # resource "aws_instance" "MariaDB_1" {
