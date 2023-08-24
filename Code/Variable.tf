6a6fb46b52941afe7bc125e8e6dbffe4338c6116
kits26kat24 <k.tijani@hotmail.com>
2023-08-24 02:10:21 +0100

    --amend

--- Code/Variable.tf
+++ Code/Variable.tf
@@ -17,23 +17,23 @@ variable "ec2_key_name" {
   default     = "Kitskat2023"
 }
 
-# variable "ec2_name_1" {
-#   description = "This is a variable to manage ec2 key name"
-#   type        = string
-#   default     = "node_01"
-# }
+variable "ec2_name_1" {
+  description = "This is a variable to manage ec2 key name"
+  type        = string
+  default     = "node_1"
+}
 
-# variable "ec2_name_2" {
-#   description = "This is a variable to manage ec2 key name"
-#   type        = string
-#   default     = "node_02"
-# }
+variable "ec2_name_2" {
+  description = "This is a variable to manage ec2 key name"
+  type        = string
+  default     = "node_2"
+}
 
-# variable "ec2_name_3" {
-#   description = "This is a variable to manage ec2 key name"
-#   type        = string
-#   default     = "MariaDB_01"
-# }
+variable "ec2_name_3" {
+  description = "This is a variable to manage ec2 key name"
+  type        = string
+  default     = "MariaDB"
+}
 
 variable "sg_name" {
   description = "This is a variable to manage ec2 key name"
@@ -120,7 +120,7 @@ variable "sit_default_tags" {
 
 variable "bucket" {}
 variable "key" {}
-# variable "env" {}
+variable "env" {}
 
 # variable "ec2_name_1" {}
 # variable "ec2_name_2" {}
