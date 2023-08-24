b66a2e8e846eb73bd9f03b8a61686972ce5578fd
kits26kat24 <k.tijani@hotmail.com>
2023-08-24 02:26:33 +0100

    --amend

--- Code/Variable.tf
+++ Code/Variable.tf
@@ -17,23 +17,23 @@ variable "ec2_key_name" {
   default     = "Kitskat2023"
 }
 
-variable "ec2_name_1" {
-  description = "This is a variable to manage ec2 key name"
-  type        = string
-  default     = "node_1"
-}
+# variable "ec2_name_1" {
+#   description = "This is a variable to manage ec2 key name"
+#   type        = string
+#   default     = "node_1"
+# }
 
-variable "ec2_name_2" {
-  description = "This is a variable to manage ec2 key name"
-  type        = string
-  default     = "node_2"
-}
+# variable "ec2_name_2" {
+#   description = "This is a variable to manage ec2 key name"
+#   type        = string
+#   default     = "node_2"
+# }
 
-variable "ec2_name_3" {
-  description = "This is a variable to manage ec2 key name"
-  type        = string
-  default     = "MariaDB"
-}
+# variable "ec2_name_3" {
+#   description = "This is a variable to manage ec2 key name"
+#   type        = string
+#   default     = "MariaDB"
+# }
 
 variable "sg_name" {
   description = "This is a variable to manage ec2 key name"
@@ -46,12 +46,6 @@ variable "subnet" {
   type        = list(any)
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
-
-# variable "public_ip" {
-#   description = "This is a variable to manage ec2 key name"
-#   type        = string
-#   default     = "10.0.3.0/24"
-# }
 
 variable "availability_zone" {
   description = "This is a variable to manage ec2 key name"
@@ -76,35 +70,35 @@ variable "ec2_sit_public_ip_3" {
   default     = "aws_instance.Legal_Node.public_ip"
 }
 
-variable "dev_default_tags_1" {
-  description = "This variable is declared here to manage ec2 tags"
-  type        = map(any)
-  default = {
-    "Name"        = "Node_1"
-    "Dept"        = "Accounting"
-    "can destroy" = "yes"
-  }
-}
+# variable "dev_default_tags_1" {
+#   description = "This variable is declared here to manage ec2 tags"
+#   type        = map(any)
+#   default = {
+#     "Name"        = "Node_1"
+#     "Dept"        = "Accounting"
+#     "can destroy" = "yes"
+#   }
+# }
 
-variable "dev_default_tags_2" {
-  description = "This variable is declared here to manage ec2 tags"
-  type        = map(any)
-  default = {
-    "Name"        = "Node_2"
-    "Dept"        = "Finance"
-    "can destroy" = "yes"
-  }
-}
+# variable "dev_default_tags_2" {
+#   description = "This variable is declared here to manage ec2 tags"
+#   type        = map(any)
+#   default = {
+#     "Name"        = "Node_2"
+#     "Dept"        = "Finance"
+#     "can destroy" = "yes"
+#   }
+# }
 
-variable "sit_default_tags" {
-  description = "This variable is declared here to manage ec2 tags"
-  type        = map(any)
-  default = {
-    "Name"        = "MariaDB"
-    "Dept"        = "Legal"
-    "can destroy" = "yes"
-  }
-}
+# variable "sit_default_tags" {
+#   description = "This variable is declared here to manage ec2 tags"
+#   type        = map(any)
+#   default = {
+#     "Name"        = "MariaDB"
+#     "Dept"        = "Legal"
+#     "can destroy" = "yes"
+#   }
+# }
 
 # variable "bucket" {
 #   description = "This is a variable to manage ec2 bucket name"
