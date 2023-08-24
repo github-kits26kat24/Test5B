5db4909404a659316069a7cd6c338cdc163cec6a
kits26kat24 <k.tijani@hotmail.com>
2023-08-24 20:06:59 +0100

    --amend

--- Code/Rds.tf
+++ Code/Rds.tf
@@ -1,36 +1,36 @@
-# resource "aws_db_subnet_group" "mariadb-sg" {
-#   name        = "mariadb-subnet"
-#   description = "RDS subnet group"
-#   subnet_ids  = [aws_subnet.public_1.id, aws_subnet.private.id]
-# }
-# resource "aws_db_parameter_group" "mariadb-pg" {
-#   name   = "mariadb-parameter"
-#   family = "mariadb10.3"
+resource "aws_db_subnet_group" "mariadb-sg" {
+  name        = "mariadb-subnet"
+  description = "RDS subnet group"
+  subnet_ids  = [aws_subnet.public_1.id, aws_subnet.private.id]
+}
+resource "aws_db_parameter_group" "mariadb-pg" {
+  name   = "mariadb-parameter"
+  family = "mariadb10.3"
 
-#   parameter {
-#     name  = "max_allowed_packet"
-#     value = "16777216"
-#   }
-# }
-# resource "aws_db_instance" "mariadb" {
-#   allocated_storage = 5 # 100 GB of storage, gives us more IOPS than a lower number
-#   engine            = "mariadb"
-#   engine_version    = "10.3"
-#   instance_class    = "db.t2.micro" # use micro if you want to use the free tier
-#   identifier             = "mariadb"
-#   db_name                = "mydatabase"    # database name
-#   username               = "kits26kat2486" # var.RDS_USERNAME # username
-#   password               = "kitskatrds"    # var.RDS_PASSWORD # password
-#   db_subnet_group_name   = aws_db_subnet_group.mariadb-sg.name
-#   parameter_group_name   = aws_db_parameter_group.mariadb-pg.name
-#   multi_az               = "false" # set to true to have high availability: 2 instances synchronized with each other
-#   vpc_security_group_ids = [aws_security_group.ec2_test5b_sg.id]
-#   storage_type           = "gp2" # ?
-#   #backup_retention_period   = 30                                          # how long you’re going to keep your backups
-#   # availability_zone         = [aws_subnet.private.availability_zone] # prefered AZ
-#   final_snapshot_identifier = "mariadb-final-snapshot" # final snapshot when executing terraform destroy
-#   skip_final_snapshot       = "true" # false If you want a snapshot of the RDS instance before it gets destroyed and recreated
-#   tags = {
-#     Name = "mariadb-engine"
-#   }
-# }
+  parameter {
+    name  = "max_allowed_packet"
+    value = "16777216"
+  }
+}
+resource "aws_db_instance" "mariadb" {
+  allocated_storage = 5 # 100 GB of storage, gives us more IOPS than a lower number
+  engine            = "mariadb"
+  engine_version    = "10.3"
+  instance_class    = "db.t2.micro" # use micro if you want to use the free tier
+  identifier             = "mariadb"
+  db_name                = "mydatabase"    # database name
+  username               = "kits26kat2486" # var.RDS_USERNAME # username
+  password               = "kitskatrds"    # var.RDS_PASSWORD # password
+  db_subnet_group_name   = aws_db_subnet_group.mariadb-sg.name
+  parameter_group_name   = aws_db_parameter_group.mariadb-pg.name
+  multi_az               = "false" # set to true to have high availability: 2 instances synchronized with each other
+  vpc_security_group_ids = [aws_security_group.ec2_test5b_sg.id]
+  storage_type           = "gp2" # ?
+  #backup_retention_period   = 30                                          # how long you’re going to keep your backups
+  # availability_zone         = [aws_subnet.private.availability_zone] # prefered AZ
+  final_snapshot_identifier = "mariadb-final-snapshot" # final snapshot when executing terraform destroy
+  skip_final_snapshot       = "true" # false If you want a snapshot of the RDS instance before it gets destroyed and recreated
+  tags = {
+    Name = "mariadb-engine"
+  }
+}
